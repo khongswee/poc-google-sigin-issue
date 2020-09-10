f889715eda4e9da5065808746e790175e11c350b
saranya.khongsawee <saranya.khongsawee>
2020-09-10 12:55:12 +0700

    Implement click open pop google sign in

--- app/src/main/java/com/poc/googlesignin/MainActivity.kt
+++ app/src/main/java/com/poc/googlesignin/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.poc.googlesignin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
 import com.poc.googlesignin.WebJavaScripInterface.Companion.TRIGGER_NATIVE
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),JavaScriptCallbacks {
+class MainActivity : AppCompatActivity(), JavaScriptCallbacks {
+    companion object {
+        private const val GOOGLE_SIGN_IN = 4432
+    }
+
+    private var mGoogleSignInClient: GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +27,7 @@ class MainActivity : AppCompatActivity(),JavaScriptCallbacks {
         loadWeb()
     }
 
-    private fun loadWeb(){
+    private fun loadWeb() {
         wvContent.also {
             it.settings.javaScriptEnabled = true
             it.settings.domStorageEnabled = true
@@ -26,7 +39,51 @@ class MainActivity : AppCompatActivity(),JavaScriptCallbacks {
     override fun handleJavaScripInterface(bridgeKey: String, value: String) {
         when (bridgeKey) {
             TRIGGER_NATIVE -> {
-                Toast.makeText(this,value,Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+                onClickGoogleSignIn()
+            }
+        }
+    }
+
+    private fun onClickGoogleSignIn() {
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestServerAuthCode(
+                    getString(R.string.default_web_client_id),
+                    true
+                )
+                .requestEmail()
+                .requestScopes(
+                    Scope(Scopes.EMAIL)
+                )
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = mGoogleSignInClient?.signInIntent
+
+        startActivityForResult(
+            signInIntent,
+            GOOGLE_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                val account = try {
+                    task.getResult(ApiException::class.java)
+                } catch (e: ApiException) {
+                    null
+                }
+                Toast.makeText(this, account?.email, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "unidentifiedError", Toast.LENGTH_SHORT).show()
             }
         }
     }
